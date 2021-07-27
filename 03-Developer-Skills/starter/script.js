ea596fff42e1e050891ea0735eb3d1bf654e1218
camzero94 <71481920+camzero94@users.noreply.github.com>
2021-07-27 18:32:13 +0800

    Delete 03-Developer-Skills directory

--- 03-Developer-Skills/starter/script.js
+++
@@ -1,9 +0,0 @@
-// Remember, we're gonna use strict mode in all scripts now!
-'use strict';
-
-const birthYear = 1991;
-
-const age = function (birthYear) {
-  return 2037 - birthYear;
-};
-console.log(25);
