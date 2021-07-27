00c38c49b0847be6d5cd5f8b8a930d7be30d2ead
camzero94 <71481920+camzero94@users.noreply.github.com>
2021-07-27 18:31:53 +0800

    Delete 01-Fundamentals-Part-1 directory

--- 01-Fundamentals-Part-1/starter/EqualityOperators.js
+++
@@ -1,9 +0,0 @@
-const favourite = prompt("Whats your favourit Number?"); // prompt returns string so Number(prompt) convert it to a number
-console.log(favourite);
-console.log(typeof favourite);
-
-if (favourite == 23) { // '23' == 23 coersion 
-    console.log('Cool 23 is an amazing number!');
-}
-
-
